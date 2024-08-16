9a5259f2de3627f0e2b343e923b7f420e1c5c49d
Japheth-hub <japheth.ra369@gmail.com>
2024-08-15 21:29:13 -0600

    Implementando web sockkets

--- src/Dashboard/dashboard.jsx
+++ src/Dashboard/dashboard.jsx
@@ -1,30 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import style from "./dashboard.module.css"
+import { socket } from '../socket'
 
 export default function Dashboard() {
 
   const navigate = useNavigate()
+  const [user, setUser] = useState()
+  const [typeSearching, setTypeSearching] = useState('Users')
+  const [listInfo, setListInfo] = useState([])
+  const [search, setSearch] = useState('')
 
-  useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem("user")) 
-    if(!user){
+  function handelTypeSearching(e){
+    setTypeSearching(e.target.value)
+    setSearch('')
+  }
+
+  function handleSearch(e){
+      setSearch(e.target.value)
+      socket.emit(`search ${typeSearching}`, search)
+  }
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    if (!user) {
       navigate("/")
     }
+    setUser(user.name)
+    socket.on('searching', (data) => {
+      console.log(data)
+      setListInfo(data.info)
+    })
   }, [])
 
   return (
     <div className={style.principalDashboard}>
-      <h3 className={style.titleDashboard}>Dashboard</h3>
-      <div className={style.contenedorDashboard}>
-        <ul className={style.menu}>
-          <li className={style.opcion}>Users</li>
-          <li className={style.opcion}>Movies</li>
-          <li className={style.opcion}>Comments</li>
-        </ul>
-        <div className={style.showInfoDashboard}>
-          Contenedro donde iran los datos dependiendo la seleccion
+      <div className={style.slidebar}>
+        <h3 className={style.titleDashboard}>Dashboard</h3>
+        <div className={style.contenedorDashboard}>
+          <ul className={style.menu}>
+            <li><h3>{user}</h3></li>
+            <li className={style.opcion}><button onClick={handelTypeSearching} value={'Users'}>Users</button></li>
+            <li className={style.opcion}><button onClick={handelTypeSearching} value={'Movies'}>Movies</button></li>
+            <li className={style.opcion}><button onClick={handelTypeSearching} value={'Comments'}>Comments</button></li>
+          </ul>
         </div>
+      </div>
+      <div className={style.showInfoDashboard}>
+        Contenedro donde iran los datos dependiendo la seleccion
+        <input type="text" value={search} onChange={handleSearch} />
+        <ul>
+          {listInfo?.map((item, i)=>{
+              return (
+                <li key={i}>{item.name || item.title}</li>
+              )
+            })
+          }
+        </ul>
       </div>
     </div>
   )
